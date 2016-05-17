0ff3b059556811761f173a37b087421392c6ff0f
sushilcs111 <sushil.sud@gmail.com>
2016-05-17 20:39:12 +0530

    fdsdfs

--- js/controllers.js
+++ js/controllers.js
@@ -5469,7 +5469,7 @@ angular.module('your_app_name.controllers', [])
                             $http({
                                 method: 'GET',
                                 url: domain + 'appointment/update-frame-rate',
-                                params: {vjhId: $scope.vjhId}
+                                 params: {vjhId: $scope.vjhId,framerate:event.stream.frameRate}
                             }).then(function sucessCallback(response) {
                                 console.log(response);
                             }, function errorCallback(e) {
