43dd82db57aa01a0a5b4f95689b6220249c2cad9
leenaNi <leena@infiniteit.biz>
2016-07-05 15:42:31 +0530

    asdsa

--- js/controllers.js
+++ js/controllers.js
@@ -4344,14 +4344,18 @@ angular.module('your_app_name.controllers', [])
             $http({
                 method: 'GET',
                 url: domain + 'doctrsrecords/get-measure-fields',
-                params: {patient: $scope.patientId, userId: $scope.userId, doctor: $scope.doctorId, catId: $scope.catId, mid: $stateParams.mid}
+                params: {patient: $scope.patientId, userId: $scope.userId, doctor: $scope.doctorId, catId: $scope.catId, mid: $stateParams.mid, recId: $scope.recId}
             }).then(function successCallback(response) {
                 console.log(response);
-                $scope.records = response.data.record;
-                $scope.fields = response.data.fields;
+                $scope.mrecords = response.data.record;
+                $scope.mfields = response.data.fields;
                 $scope.editRec = response.data.editRec;
                 $scope.abt = response.data.abt;
                 $scope.measurement = response.data.measurement;
+                $scope.mid = response.data.mid;
+                if (response.data.mid.length > 0) {
+                    $scope.measure = 'yes';
+                }
             }, function errorCallback(response) {
                 console.log(response);
             });
@@ -4371,6 +4375,30 @@ angular.module('your_app_name.controllers', [])
                     } else if (response.err != '') {
                         alert('Please fill mandatory fields');
                     }
+                });
+            };
+            $rootScope.$on("GetMeasurements", function () {
+                $scope.getMeasurements();
+            });
+            $scope.getMeasurements = function () {
+                console.log('Get note measures');
+                $http({
+                    method: 'GET',
+                    url: domain + 'doctrsrecords/get-measure-fields',
+                    params: {patient: $scope.patientId, userId: $scope.userId, doctor: $scope.doctorId, catId: $scope.catId, mid: '', recId: $scope.recId}
+                }).then(function successCallback(response) {
+                    //console.log(response);
+                    $scope.mrecords = response.data.record;
+                    $scope.mfields = response.data.fields;
+                    $scope.editRec = response.data.editRec;
+                    $scope.abt = response.data.abt;
+                    $scope.measurement = response.data.measurement;
+                    $scope.mid = response.data.mid;
+                    if (response.data.mid.length > 0) {
+                        $scope.measure = 'yes';
+                    }
+                }, function errorCallback(response) {
+                    console.log(response);
                 });
             };
         })
@@ -6619,28 +6647,36 @@ angular.module('your_app_name.controllers', [])
             $scope.mntab = 'ntcase';
             $scope.nadd = 'null';
             $scope.notetcase = true;
+            $scope.MeasurementCards = true;
+            $scope.TestResultsCards = true;
+            $scope.ObservationCards = true;
+            $scope.DiagnosisCard = true;
             $scope.changecate = function (ab) {
-
                 //$scope.obervation=false;
                 if (ab == 'testresults') {
                     $scope.ttestresults = true;
                     $scope.tmeasurements = false;
                     $scope.tobervation = false;
+                    $scope.tdiagnosis = false;
                 } else if (ab == 'measurement') {
                     $scope.tmeasurements = true;
                     $scope.ttestresults = false;
                     $scope.tobervation = false;
-
+                    $scope.tdiagnosis = false;
                 } else if (ab == 'obervation') {
                     $scope.tobervation = true;
                     $scope.ttestresults = false;
                     $scope.tmeasurements = false;
-
+                    $scope.tdiagnosis = false;
+                } else if (ab == 'diagnosis') {
+                    $scope.tdiagnosis = true;
+                    $scope.tobervation = false;
+                    $scope.ttestresults = false;
+                    $scope.tmeasurements = false;
                 }
             }
 
             $scope.changemaincate = function (cvalue) {
-
                 if (cvalue == 'ntcase') {
                     $scope.notetcase = true
                     $scope.notebackground = false
@@ -6657,14 +6693,13 @@ angular.module('your_app_name.controllers', [])
                     $scope.notetnote = true
                     $scope.notetreatment = false
                 }
-
                 if (cvalue == 'nttreatment') {
                     $scope.notetcase = false
                     $scope.notebackground = false
                     $scope.notetnote = false
                     $scope.notetreatment = true
                 }
-            }
+            };
 
             $scope.curDate = $filter('date')(new Date(), 'yyyy-MM-dd HH:mm:ss');
             var imgCnt = 0;
@@ -6684,6 +6719,20 @@ angular.module('your_app_name.controllers', [])
             $scope.cnAttachments = '';
             $scope.medicinename = '';
             $scope.prescription = 'No';
+            $scope.measure = '';
+            $scope.measurement = {};
+            $scope.diaId = '';
+            $scope.diaText = {value: ''};
+            $scope.diaTextValue = '';
+            $scope.testId = '';
+            $scope.testResult = [];
+            $scope.testTextValue = '';
+            $scope.testSum = '';
+            $scope.objId = '';
+            $scope.objText = [];
+            $scope.objTextValue = '';
+            $scope.observation = {};
+            $scope.objSum = '';
             var stoppedTimer;
             $scope.Timercounter;
             var statstimer;
@@ -6953,6 +7002,7 @@ angular.module('your_app_name.controllers', [])
                     $scope.doctorId = response.data.doctr.id
                     $scope.app = response.data.app;
                     $scope.prevRecord = response.data.record;
+                    $scope.recId = response.data.record.id;
                     $scope.prevRecordDetails = response.data.recordDetails;
                     if ($scope.prevRecordDetails.length > 0) {
                         angular.forEach($scope.prevRecordDetails, function (val, key) {
@@ -6965,7 +7015,6 @@ angular.module('your_app_name.controllers', [])
                                 $scope.isAttachment = val.attachments.length;
                             }
                         });
-                        $scope.recId = response.data.record.id;
                     }
                     if (response.data.app.mode == 1) {
                         $scope.mode = 'Video';
@@ -6983,6 +7032,7 @@ angular.module('your_app_name.controllers', [])
                     window.localStorage.setItem('doctorId', $scope.doctorId);
                     console.log($scope.conDate);
                     $scope.getEvaluationDetails();
+                    $rootScope.$emit("GetMeasurements", {});
                     $http({
                         method: 'GET',
                         url: domain + 'doctrsrecords/get-fields',
@@ -7364,18 +7414,6 @@ angular.module('your_app_name.controllers', [])
                 });
                 $http({
                     method: 'GET',
-                    url: domain + 'doctrsrecords/get-diagnosis-lang',
-                    params: {userId: $scope.userId, diaId: $scope.diaId, recId: $scope.recId}
-                }).then(function successCallback(response) {
-                    if (response.data.recdata != '') {
-                        $scope.diaId = response.data.recdata.record_id;
-                        $scope.diaText.value = response.data.recdata.value;
-                    }
-                }, function errorCallback(e) {
-                    console.log(e);
-                });
-                $http({
-                    method: 'GET',
                     url: domain + 'doctrsrecords/get-observations-lang',
                     params: {userId: $scope.userId, interface: $scope.interface, objId: $scope.objId, recId: $scope.recId}
                 }).then(function successCallback(response) {
@@ -7384,6 +7422,18 @@ angular.module('your_app_name.controllers', [])
                         $scope.objId = response.data.recdata.record_id;
                         $scope.observation = response.data.recdata;
                         $scope.objText = response.data.recdata.metadata_values;
+                    }
+                }, function errorCallback(e) {
+                    console.log(e);
+                });
+                $http({
+                    method: 'GET',
+                    url: domain + 'doctrsrecords/get-diagnosis-lang',
+                    params: {userId: $scope.userId, diaId: $scope.diaId, recId: $scope.recId}
+                }).then(function successCallback(response) {
+                    if (response.data.recdata != '') {
+                        $scope.diaId = response.data.recdata.record_id;
+                        $scope.diaText.value = response.data.recdata.value;
                     }
                 }, function errorCallback(e) {
                     console.log(e);
